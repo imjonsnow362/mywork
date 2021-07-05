241e4d11eeb221d84b59a1355b0fa7c238131552
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2021-07-05 22:24:56 +0530

    Create fds_stack_using_ll.cpp

---
+++ fds_stack_using_ll.cpp
@@ -0,0 +1,103 @@
+//working code 
+#include<iostream>
+using namespace std;
+//defining struct for node 
+struct node
+{
+    int data;
+    node *next;
+};
+//the class
+class stack 
+{
+    node *top;
+    public : 
+            stack();
+            bool itisempty();
+            void push(int value);
+            void pop();
+            void display();
+};
+
+stack::stack()
+{
+    top=NULL;
+}
+//defining the push function 
+void stack::push(int value)
+{
+    node *ptr=new node();
+    cout<<"Enter the no you want to insert"<<endl;
+    cin>>value;
+    ptr->data=value;
+    ptr->next=top;
+    top=ptr;
+}
+//defining empty function
+bool stack::itisempty()
+{
+    if(top==NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+//defining pop function to remove the element
+void stack::pop()
+{
+    if(itisempty())
+    {
+        cout<<"EMPTY"<<endl;
+    }
+    else
+    {
+        struct node *temp=top;
+        top=top->next;
+        delete(temp);
+    }
+}
+//defining dispkay function here..
+void stack::display()
+{
+    if(itisempty())
+    {
+        cout<<"EMPTY"<<endl;
+    }
+    else
+    {
+        cout<<top->data;
+    }
+}
+
+int main()
+{
+    stack object;
+    int ch;
+    int value;
+    do{
+        cout<<"Choose!!"<<endl;
+        cout<<"\n1.PUSH  \n2.POP  \n3.DISPLAY"<<endl;
+        cin>>ch;
+        switch(ch)
+        {
+            case 1:
+                    object.push(value);
+                    cout<<"PUSHED ELEMENT IN THE STACK ARE----"<<endl;
+                    break;
+            case 2:
+                    object.pop();
+                    cout<<"ELEMENT POPPED---"<<endl;
+                    break;
+            case 3:
+                    cout<<"DISPLAY--"<<endl;
+                    object.display();
+                    cout<<"\n";
+                    break;
+            default:
+                    cout<<"WRONG CHOICE!!!"<<endl;
+        }
+    }while(ch!=4);
+}
